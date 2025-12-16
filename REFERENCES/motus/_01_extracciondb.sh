5084d3020a0cf196b0944618167d0de2ccdf369e
carmeniborra <carmeniborra10@gmail.com>
2025-12-16 11:44:28 +0100

    Modified references/motus

--- REFERENCES/motus/_01_extracciondb.sh
+++ REFERENCES/motus/_01_extracciondb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-# 1. Verificar tamano del archivo descargado (~3 GB)
+# 1. Verificar tamano del archivo descargado
 echo "Tamano de la base de datos:"
 ls -lh /data/ucct/bi/research/20250625_TFM_CIBORRA_IC-SM_T/tmp_motus/db_mOTU.tar.gz
 
