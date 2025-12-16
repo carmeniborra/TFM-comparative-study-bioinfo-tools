7b15c7f0e575938c003e6aa436c19585a68a3f98
carmeniborra <carmeniborra10@gmail.com>
2025-12-16 12:28:43 +0100

    Renamed analysis

--- ANALYSIS/20251121_ANALYSIS16_ERP015409_diamond/_02_to_genus.sh
+++
@@ -1,2 +0,0 @@
-module load Python
-python ../../DOC/to_genus.py results/taxpasta/        --pattern "*.tsv"        --taxdump-dir ../../REFERENCES/taxdump/2025-09-01_taxdump        --outdir results/taxpasta/filtered
--- ANALYSIS/20251121_ANALYSIS16_ERP015409_diamond/_03_rel_abundance.sh
+++
@@ -1,2 +0,0 @@
-module load Python
-python ../../DOC/rel_abundance.py results/taxpasta/filtered/         --pattern "*.tsv" --outdir results/taxpasta/rel_abundance         --level genus --study-wide         --sample-trim _
