eaef202255d26b08d4fcc0452101313ae04d30a3
carmeniborra <carmeniborra10@gmail.com>
2026-01-13 18:25:34 +0100

    Adding diamond1.1

---
+++ ANALYSIS/20251121_ANALYSIS02_diamond1.1/_03_rel_abundance.sh
@@ -0,0 +1,2 @@
+module load Python
+python ../../DOC/rel_abundance.py results/taxpasta/filtered/         --pattern "*.tsv" --outdir results/taxpasta/rel_abundance         --level genus --study-wide         --sample-trim _
